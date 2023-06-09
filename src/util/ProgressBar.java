72e90372cb27e96fa7fad85701b8803b4b6e3ed7
Vinícius Reis <vnc.reis@outlook.com>
2023-06-09 16:38:24 -0300

    Add progress bar test

--- src/util/ProgressBar.java
+++ src/util/ProgressBar.java
@@ -4,26 +4,46 @@ public class ProgressBar {
     private static final int SIZE = 20;
     private static final String PROGRESS_TOKEN = "=";
     private static final String END_PROGRESS_TOKEN = ">";
+    private final String name;
     private final String leftText;
     private final long total;
+    private long current = 0;
     private double progress;
 
-    public ProgressBar(long total, String leftText){
+    public ProgressBar(String name, long total, String leftText){
+        this.name = name;
         this.total = total;
         this.leftText = leftText;
     }
 
+    public static void main(String[] args) {
+        try {
+            final ProgressBar progress = new ProgressBar("Test", 100, "Nothing...");
+
+            for (int i = 0; i < 100; i++) {
+                Thread.sleep(1500);
+
+                progress.update(i);
+                progress.print();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public void update(long value) {
-        progress = (double) total / value;
+        current = value;
+        progress = Math.min(1.0, (double) current / total);
     }
 
     public void print() {
-        // FIXME: Not working during the update
-
         final long filledSize = Math.round(SIZE * progress);
-        final long unfilledSize = Math.round(1 - SIZE * progress);
+        final long unfilledSize = SIZE - filledSize;
 
-        if(leftText != null && leftText.isEmpty())
+        if(name != null && !name.isEmpty())
+            System.out.printf("[%s] ", name);
+
+        if(leftText != null && !leftText.isEmpty())
             System.out.print(leftText + " ");
 
         System.out.print("|");
@@ -37,6 +57,6 @@ public class ProgressBar {
         for (int i = 0; i < unfilledSize; i++)
             System.out.print(" ");
 
-        System.out.printf("| %.2f %%\r", progress * 100);
+        System.out.printf("| (%d, %d) %.2f %%\r", current, total, progress * 100);
     }
 }
